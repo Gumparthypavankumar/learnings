8d9e62d8f137fe3d32049576bfb5986236d4faaa
Gumparthy Pavan Kumar[pk] <gumparthypk@gmail.com>
2024-06-08 22:54:32 +0530

    test(P-10): add tests for i8n

--- java/pavan/i8n/src/test/java/com/pavan/i8n/I8nApplicationTest.java
+++ java/pavan/i8n/src/test/java/com/pavan/i8n/I8nApplicationTest.java
@@ -1,18 +1,45 @@
 package com.pavan.i8n;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
  * The type I8nApplicationTest.
  *
  * @author gumparthypavankumar[pk] created on 08/06/24
  */
-@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {I8nApplication.class})
+@AutoConfigureMockMvc
 class I8nApplicationTest {
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  void contextLoads() {
+  void contextLoads() throws Exception {
+
+    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/i8n")
+        .header("Accept-Language", "it")
+        .queryParam("key", "App.Name");
+    MockHttpServletResponse response =
+        this.mockMvc.perform(requestBuilder).andReturn().getResponse();
+    Assertions.assertEquals(200, response.getStatus());
+    Assertions.assertTrue(response.getContentAsString().contains("internazionalizzazione"));
+
+    requestBuilder = MockMvcRequestBuilders.get("/i8n")
+        .header("Accept-Language", "en")
+        .queryParam("key", "App.Name");
+    response =
+        this.mockMvc.perform(requestBuilder).andReturn().getResponse();
+    Assertions.assertEquals(200, response.getStatus());
+    Assertions.assertTrue(response.getContentAsString().contains("Internationalization"));
+
   }
 }
